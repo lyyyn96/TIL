b633ff85dda889be856e3e24b0a570c23bc11599
ChoiHyeRin <lyyyn@naver.com>
2021-02-09 18:32:54 +0900

    Add Thread

---
+++ Basic_ch12/src/test/thread/Test.java
@@ -0,0 +1,43 @@
+package test.thread;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		A t1 = new A();
+		A t2 = new A();
+		
+		Thread th1 = new Thread(t1);
+		Thread th2 = new Thread(t2);
+		
+		th1.start();
+		th2.start();
+		
+		/*
+		//t1 thread
+		t1.start();
+		//t2 thread
+		t2.start();
+		*/
+	}
+}
+
+//이미 다른 class를 상속받았다면
+//Runnable interface를 사용
+//이 방법이 더 OOP적이다.
+class A implements Runnable{
+	public void run() {
+		for(int i=0; i<100; i++) {
+			System.out.println(i+"+1="+(i+1));
+		}
+	}
+}
+
+/*
+class A extends Thread{
+	public void run() {
+		for(int i=0; i<100; i++) {
+			System.out.println(i+"+1="+(i+1));
+		}
+	}
+}
+*/
