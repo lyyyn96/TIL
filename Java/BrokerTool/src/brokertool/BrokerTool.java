5ffeb0676fb3fd2b7dcdc15c9828fcfbf88fb098
ChoiHyeRin <lyyyn@naver.com>
2021-02-22 18:10:55 +0900

    Add brokertool

---
+++ Java/BrokerTool/src/brokertool/BrokerTool.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package brokertool;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author lyyyn
+ */
+public class BrokerTool {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JFrame.setDefaultLookAndFeelDecorated(true);
+        new BrokerUI().setVisible(true);
+    }
+    
+}
