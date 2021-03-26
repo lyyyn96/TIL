0be7f7f302dc263e7e4ac55ebe60800f4899832a
ChoiHyeRin <lyyyn@naver.com>
2021-03-26 18:15:42 +0900

    Add Cafe Web Application with SpringBoot and MyBatis

--- Web/SpringBoot_MyBatis_Cafe2/src/main/java/com/mulcam/ai/web/service/OrderService.java
+++ Web/SpringBoot_MyBatis_Cafe2/src/main/java/com/mulcam/ai/web/service/OrderService.java
@@ -1,7 +1,10 @@
 package com.mulcam.ai.web.service;
 
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,14 +24,56 @@ public class OrderService {
 	public OrderService() {
 		try {
 			ss = new ServerSocket(9999);
-			ss.accept();
+			
+			new Thread(()-> {
+					while(true) {
+						try {
+							Socket s = ss.accept();
+							ObjectOutputStream out = new ObjectOutputStream(s.getOutputStream());
+							ObjectInputStream in = new ObjectInputStream(s.getInputStream());
+							new KitchenThread(s, in, out).start();
+						}catch(IOException e) {
+							e.printStackTrace();
+						}
+					}
+				}
+			).start();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 	
+	//주문 입력 받기
 	public long insert(ArrayList<OrderVO> list){
-		return orderDAO.insert(list);
+		long order_group_no = orderDAO.insert(list);
+		//주방으로 주문 통보
+		out.writeObject();
+		
+		return order_group_no;
 	}
+	
+	private class KitchenThread extends Thread{
+		
+		Socket s; 
+		ObjectInputStream in;
+		ObjectOutputStream out;
+		
+		public KitchenThread(Socket s, ObjectInputStream in, ObjectOutputStream out) {
+			this.s = s;
+			this.in = in;
+			this.out = out;
+		}
 
-}
+		@Override
+		public void run() {
+			try { //while문 밖에서 try-catch
+				while(true) {
+					in.readObject();
+				}
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+}//end OrderService
