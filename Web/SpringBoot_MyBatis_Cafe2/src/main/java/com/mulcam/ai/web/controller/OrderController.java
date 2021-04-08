3d737beea39d2f91ff3491d20b49ac4b6ba39d83
ChoiHyeRin <lyyyn@naver.com>
2021-04-08 18:13:24 +0900

    Add processing order by Kiosk

--- Web/SpringBoot_MyBatis_Cafe2/src/main/java/com/mulcam/ai/web/controller/OrderController.java
+++ Web/SpringBoot_MyBatis_Cafe2/src/main/java/com/mulcam/ai/web/controller/OrderController.java
@@ -1,7 +1,11 @@
 package com.mulcam.ai.web.controller;
 
 import java.io.BufferedReader;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,6 +16,7 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -47,7 +52,7 @@ public class OrderController {
 		return json.toJSONString();
 	}
 	
-	///////////// 주문 처리 //////////////////
+	///////////// 주문 처리(web) //////////////////
 	@RequestMapping(value = "order.chr", 
 			method= {RequestMethod.POST},
 			produces = "application/text; charset=utf8")
@@ -85,7 +90,7 @@ public class OrderController {
 			
 			json=new JSONObject();			
 			
-			if(true) {		
+			if (order_group_no>0) {
 				json.put("order_group_no", order_group_no);
 			}else {
 				json.put("msg", "주문 실패");
@@ -95,6 +100,43 @@ public class OrderController {
 			json.put("msg", e.getMessage());
 		}	
 		return json.toJSONString();		
+	}
+	
+	///////////// 주문 처리2(kiosk) //////////////////
+	@RequestMapping(value = "order2.chr", method = { RequestMethod.POST }, consumes = "application/json;charset=utf-8")
+
+	@ResponseBody
+	public String order2(@RequestBody List<Map<String, Object>> param) {
+		JSONObject json = null;
+
+		ArrayList<OrderVO> list = new ArrayList<OrderVO>();
+		for (Map<String, Object> map : param) {
+			String prodname = (String) map.get("name");
+			// System.out.println(rawName);
+
+			try {
+				prodname = URLDecoder.decode(prodname, "UTF-8");
+				// System.out.println(name);
+			} catch (UnsupportedEncodingException e) {
+				
+			}
+			
+			Long quantity = Long.parseLong((String) (map.get("quantity")));
+			OrderVO orderVO = new OrderVO("kiosk", prodname, quantity);
+			list.add(orderVO);
+		}
+
+		System.out.println("총 " + list.size() + "개 품목을 주문합니다");
+		long order_group_no = orderService.insert(list);
+		System.out.println(order_group_no);
+		json = new JSONObject();
+
+		if (order_group_no > 0) {
+			json.put("order_group_no", order_group_no);
+		} else {
+			json.put("msg", "주문 실패");
+		}
+		return json.toJSONString();
 	}
 
 }
